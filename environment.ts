ada8c29975348eb2a51e2256b8526e7ca2776bc6
Sergey <2901744+evercoinx@users.noreply.github.com>
2024-08-19 12:29:11 +0200

    Format configuration and environment files

--- environment.ts
+++ environment.ts
@@ -1,46 +1,45 @@
-import * as dotenv from 'dotenv'
-import * as path from 'path'
+import * as dotenv from "dotenv";
+import * as path from "path";
+import { ethers } from "ethers";
 
-import { ethers } from 'ethers'
+const envSuffix = process.env.NODE_ENV === "main" ? "" : ".test";
 
-const envSuffix = process.env.NODE_ENV === 'main' ? '' : '.test'
-
-dotenv.config({ path: path.join(__dirname, '.env' + envSuffix) })
+dotenv.config({ path: path.join(__dirname, ".env" + envSuffix) });
 
 const DEPLOYER_PRIVATE_KEY =
-  process.env.DEPLOYER_PRIVATE_KEY || ethers.Wallet.createRandom().privateKey
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ''
-const ROOT_CHAIN_RPC = process.env.ROOT_CHAIN_RPC || ''
-const CHILD_CHAIN_RPC = process.env.CHILD_CHAIN_RPC || ''
-const STAKE_MANAGER = process.env.STAKE_MANAGER || ''
-const MATIC_TOKEN = process.env.MATIC_TOKEN || ''
-const MANAGER = process.env.MANAGER || ''
-const INSTANT_POOL_OWNER = process.env.INSTANT_POOL_OWNER || ''
-const TREASURY = process.env.TREASURY || ''
-const ROOT_GAS_PRICE = process.env.ROOT_GAS_PRICE || 0
-const CHILD_GAS_PRICE = process.env.CHILD_GAS_PRICE || 0
-const DEFENDER_TEAM_API_KEY = process.env.DEFENDER_TEAM_API_KEY || ''
+	process.env.DEPLOYER_PRIVATE_KEY || ethers.Wallet.createRandom().privateKey;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const ROOT_CHAIN_RPC = process.env.ROOT_CHAIN_RPC || "";
+const CHILD_CHAIN_RPC = process.env.CHILD_CHAIN_RPC || "";
+const STAKE_MANAGER = process.env.STAKE_MANAGER || "";
+const MATIC_TOKEN = process.env.MATIC_TOKEN || "";
+const MANAGER = process.env.MANAGER || "";
+const INSTANT_POOL_OWNER = process.env.INSTANT_POOL_OWNER || "";
+const TREASURY = process.env.TREASURY || "";
+const ROOT_GAS_PRICE = process.env.ROOT_GAS_PRICE || 0;
+const CHILD_GAS_PRICE = process.env.CHILD_GAS_PRICE || 0;
+const DEFENDER_TEAM_API_KEY = process.env.DEFENDER_TEAM_API_KEY || "";
 const DEFENDER_TEAM_API_SECRET_KEY =
-  process.env.DEFENDER_TEAM_API_SECRET_KEY || ''
-const FX_ROOT = process.env.FX_ROOT || ''
-const FX_CHILD = process.env.FX_CHILD || ''
-const CHECKPOINT_MANAGER = process.env.CHECKPOINT_MANAGER || ''
+	process.env.DEFENDER_TEAM_API_SECRET_KEY || "";
+const FX_ROOT = process.env.FX_ROOT || "";
+const FX_CHILD = process.env.FX_CHILD || "";
+const CHECKPOINT_MANAGER = process.env.CHECKPOINT_MANAGER || "";
 
 export {
-  DEPLOYER_PRIVATE_KEY,
-  ETHERSCAN_API_KEY,
-  ROOT_CHAIN_RPC,
-  CHILD_CHAIN_RPC,
-  STAKE_MANAGER,
-  MATIC_TOKEN,
-  MANAGER,
-  INSTANT_POOL_OWNER,
-  TREASURY,
-  ROOT_GAS_PRICE,
-  CHILD_GAS_PRICE,
-  DEFENDER_TEAM_API_KEY,
-  DEFENDER_TEAM_API_SECRET_KEY,
-  FX_ROOT,
-  FX_CHILD,
-  CHECKPOINT_MANAGER,
-}
+	DEPLOYER_PRIVATE_KEY,
+	ETHERSCAN_API_KEY,
+	ROOT_CHAIN_RPC,
+	CHILD_CHAIN_RPC,
+	STAKE_MANAGER,
+	MATIC_TOKEN,
+	MANAGER,
+	INSTANT_POOL_OWNER,
+	TREASURY,
+	ROOT_GAS_PRICE,
+	CHILD_GAS_PRICE,
+	DEFENDER_TEAM_API_KEY,
+	DEFENDER_TEAM_API_SECRET_KEY,
+	FX_ROOT,
+	FX_CHILD,
+	CHECKPOINT_MANAGER,
+};
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,165 +1,191 @@
-import { HardhatRuntimeEnvironment } from 'hardhat/types'
-import { HardhatUserConfig, task } from 'hardhat/config'
-
-import 'hardhat-contract-sizer'
-import 'hardhat-gas-reporter'
-import '@typechain/hardhat'
-import '@nomiclabs/hardhat-waffle'
-import '@nomiclabs/hardhat-ethers'
-import '@openzeppelin/hardhat-upgrades'
-import '@nomiclabs/hardhat-etherscan'
-import '@openzeppelin/hardhat-defender'
-
-import { deployDirect, deployProxy, verify } from './scripts/tasks'
-
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { HardhatUserConfig, task } from "hardhat/config";
+import "hardhat-contract-sizer";
+import "hardhat-gas-reporter";
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+import "@openzeppelin/hardhat-upgrades";
+import "@nomiclabs/hardhat-etherscan";
+import "@openzeppelin/hardhat-defender";
+import { deployDirect, deployProxy, verify } from "./scripts/tasks";
 import {
-  DEPLOYER_PRIVATE_KEY,
-  ETHERSCAN_API_KEY,
-  ROOT_CHAIN_RPC,
-  ROOT_GAS_PRICE,
-  DEFENDER_TEAM_API_KEY,
-  DEFENDER_TEAM_API_SECRET_KEY,
-  CHILD_CHAIN_RPC,
-  CHILD_GAS_PRICE,
-  FX_ROOT,
-  FX_CHILD,
-  CHECKPOINT_MANAGER,
-} from './environment'
+	DEPLOYER_PRIVATE_KEY,
+	ETHERSCAN_API_KEY,
+	ROOT_CHAIN_RPC,
+	ROOT_GAS_PRICE,
+	DEFENDER_TEAM_API_KEY,
+	DEFENDER_TEAM_API_SECRET_KEY,
+	CHILD_CHAIN_RPC,
+	CHILD_GAS_PRICE,
+	FX_ROOT,
+	FX_CHILD,
+	CHECKPOINT_MANAGER,
+} from "./environment";
 
-task('verifyMaticX', 'MaticX contracts verification').setAction(
-  async (args, hre: HardhatRuntimeEnvironment) => {
-    await verify(hre)
-  },
-)
+task("verifyMaticX", "MaticX contracts verification").setAction(
+	async (args, hre: HardhatRuntimeEnvironment) => {
+		await verify(hre);
+	}
+);
 
-task("deployFxStateChildTunnel", "Deploy FxStateChildTunnel")
-  .setAction(async (args, hre: HardhatRuntimeEnvironment) => {
-    if (!isChildNetwork(hre.network.name)) return
-    await deployDirect(hre, "FxStateChildTunnel", FX_CHILD)
-  }
+task("deployFxStateChildTunnel", "Deploy FxStateChildTunnel").setAction(
+	async (args, hre: HardhatRuntimeEnvironment) => {
+		if (!isChildNetwork(hre.network.name)) return;
+		await deployDirect(hre, "FxStateChildTunnel", FX_CHILD);
+	}
 );
 
 task("deployFxStateRootTunnel", "Deploy FxStateRootTunnel")
-  .addPositionalParam("maticX")
-  .setAction(async ({maticX}, hre: HardhatRuntimeEnvironment) => {
-    if (!isRootNetwork(hre.network.name)) return
-    await deployDirect(hre, "FxStateRootTunnel", CHECKPOINT_MANAGER, FX_ROOT, maticX)
-  }
-);
+	.addPositionalParam("maticX")
+	.setAction(async ({ maticX }, hre: HardhatRuntimeEnvironment) => {
+		if (!isRootNetwork(hre.network.name)) return;
+		await deployDirect(
+			hre,
+			"FxStateRootTunnel",
+			CHECKPOINT_MANAGER,
+			FX_ROOT,
+			maticX
+		);
+	});
 
 task("deployRateProvider", "Deploy RateProvider")
-  .addPositionalParam("fxStateChildTunnel")
-  .setAction(async ({fxStateChildTunnel}, hre: HardhatRuntimeEnvironment) => {
-    if (!isChildNetwork(hre.network.name)) return
-    await deployDirect(hre, "RateProvider", fxStateChildTunnel)
-  }
+	.addPositionalParam("fxStateChildTunnel")
+	.setAction(
+		async ({ fxStateChildTunnel }, hre: HardhatRuntimeEnvironment) => {
+			if (!isChildNetwork(hre.network.name)) return;
+			await deployDirect(hre, "RateProvider", fxStateChildTunnel);
+		}
+	);
+
+task("deployMaticXImpl", "Deploy MaticX Implementation only").setAction(
+	async (args, hre: HardhatRuntimeEnvironment) => {
+		if (!isRootNetwork(hre.network.name)) return;
+		await deployDirect(hre, "MaticX");
+	}
 );
 
-task("deployMaticXImpl", "Deploy MaticX Implementation only")
-  .setAction(async (args, hre: HardhatRuntimeEnvironment) => {
-    if (!isRootNetwork(hre.network.name)) return
-    await deployDirect(hre, "MaticX")
-  }
-);
-
-task("deployValidatorRegistryImpl", "Deploy ValidatorRegistry Implementation only")
-  .setAction(async (args, hre: HardhatRuntimeEnvironment) => {
-    if (!isRootNetwork(hre.network.name)) return
-    await deployDirect(hre, "ValidatorRegistry")
-  }
-);
+task(
+	"deployValidatorRegistryImpl",
+	"Deploy ValidatorRegistry Implementation only"
+).setAction(async (args, hre: HardhatRuntimeEnvironment) => {
+	if (!isRootNetwork(hre.network.name)) return;
+	await deployDirect(hre, "ValidatorRegistry");
+});
 
 task("deployChildPoolProxy", "Deploy ChildPool Proxy only")
-  .addPositionalParam("fxStateChildTunnel")
-  .addPositionalParam("maticX")
-  .addPositionalParam("manager")
-  .addPositionalParam("instantPoolOwner")
-  .addPositionalParam("treasury")
-  .addPositionalParam("instantWithdrawalFeeBps")
-  .setAction(async ({fxStateChildTunnel, maticX, manager, instantPoolOwner, treasury, instantWithdrawalFeeBps}, hre: HardhatRuntimeEnvironment) => {
-    if (!isChildNetwork(hre.network.name)) return
-    await deployProxy(hre, "ChildPool", fxStateChildTunnel, maticX, manager, instantPoolOwner, treasury, instantWithdrawalFeeBps)
-  }
-);
+	.addPositionalParam("fxStateChildTunnel")
+	.addPositionalParam("maticX")
+	.addPositionalParam("manager")
+	.addPositionalParam("instantPoolOwner")
+	.addPositionalParam("treasury")
+	.addPositionalParam("instantWithdrawalFeeBps")
+	.setAction(
+		async (
+			{
+				fxStateChildTunnel,
+				maticX,
+				manager,
+				instantPoolOwner,
+				treasury,
+				instantWithdrawalFeeBps,
+			},
+			hre: HardhatRuntimeEnvironment
+		) => {
+			if (!isChildNetwork(hre.network.name)) return;
+			await deployProxy(
+				hre,
+				"ChildPool",
+				fxStateChildTunnel,
+				maticX,
+				manager,
+				instantPoolOwner,
+				treasury,
+				instantWithdrawalFeeBps
+			);
+		}
+	);
 
-task("deployChildPoolImpl", "Deploy ChildPool Implementation only")
-  .setAction(async (args, hre: HardhatRuntimeEnvironment) => {
-    if (!isChildNetwork(hre.network.name)) return
-    await deployDirect(hre, "ChildPool")
-  }
+task("deployChildPoolImpl", "Deploy ChildPool Implementation only").setAction(
+	async (args, hre: HardhatRuntimeEnvironment) => {
+		if (!isChildNetwork(hre.network.name)) return;
+		await deployDirect(hre, "ChildPool");
+	}
 );
 
 function isChildNetwork(selected: string) {
-  const expected = 'matic';
-  return _isCorrectNetwork(expected, selected);
+	const expected = "matic";
+	return _isCorrectNetwork(expected, selected);
 }
 
 function isRootNetwork(selected: string) {
-  const expected = 'mainnet';
-  return _isCorrectNetwork(expected, selected);
+	const expected = "mainnet";
+	return _isCorrectNetwork(expected, selected);
 }
 
 function _isCorrectNetwork(expected: string, selected: string) {
-  if (selected === expected) return true;
+	if (selected === expected) return true;
 
-  console.log(`Wrong network configuration! Expected: ${expected} Selected: ${selected}`)
+	console.log(
+		`Wrong network configuration! Expected: ${expected} Selected: ${selected}`
+	);
 }
 
 const config: HardhatUserConfig = {
-  defaultNetwork: 'hardhat',
-  solidity: {
-    version: '0.8.7',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
-  networks: {
-    localhost: {
-      url: 'http://127.0.0.1:8545',
-    },
-    testnet: {
-      url: ROOT_CHAIN_RPC,
-      accounts: [DEPLOYER_PRIVATE_KEY],
-      gasPrice: Number(ROOT_GAS_PRICE),
-    },
-    mainnet: {
-      url: ROOT_CHAIN_RPC,
-      accounts: [DEPLOYER_PRIVATE_KEY],
-      gasPrice: Number(ROOT_GAS_PRICE),
-    },
-    matic: {
-      url: CHILD_CHAIN_RPC,
-      accounts: [DEPLOYER_PRIVATE_KEY],
-      gasPrice: Number(CHILD_GAS_PRICE),
-    },
-  },
-  typechain: {
-    outDir: 'typechain',
-    target: 'ethers-v5',
-  },
-  mocha: {
-    timeout: 99999999,
-  },
-  etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
-  },
-  defender: {
-    apiKey: DEFENDER_TEAM_API_KEY,
-    apiSecret: DEFENDER_TEAM_API_SECRET_KEY,
-  },
-  contractSizer: {
-    alphaSort: true,
-    disambiguatePaths: false,
-    runOnCompile: true,
-    strict: true,
-  },
-  gasReporter: {
-    currency: 'USD',
-    gasPrice: 50,
-  },
-}
+	defaultNetwork: "hardhat",
+	solidity: {
+		version: "0.8.7",
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 200,
+			},
+		},
+	},
+	networks: {
+		localhost: {
+			url: "http://127.0.0.1:8545",
+		},
+		testnet: {
+			url: ROOT_CHAIN_RPC,
+			accounts: [DEPLOYER_PRIVATE_KEY],
+			gasPrice: Number(ROOT_GAS_PRICE),
+		},
+		mainnet: {
+			url: ROOT_CHAIN_RPC,
+			accounts: [DEPLOYER_PRIVATE_KEY],
+			gasPrice: Number(ROOT_GAS_PRICE),
+		},
+		matic: {
+			url: CHILD_CHAIN_RPC,
+			accounts: [DEPLOYER_PRIVATE_KEY],
+			gasPrice: Number(CHILD_GAS_PRICE),
+		},
+	},
+	typechain: {
+		outDir: "typechain",
+		target: "ethers-v5",
+	},
+	mocha: {
+		timeout: 99999999,
+	},
+	etherscan: {
+		apiKey: ETHERSCAN_API_KEY,
+	},
+	defender: {
+		apiKey: DEFENDER_TEAM_API_KEY,
+		apiSecret: DEFENDER_TEAM_API_SECRET_KEY,
+	},
+	contractSizer: {
+		alphaSort: true,
+		disambiguatePaths: false,
+		runOnCompile: true,
+		strict: true,
+	},
+	gasReporter: {
+		currency: "USD",
+		gasPrice: 50,
+	},
+};
 
-export default config
+export default config;
