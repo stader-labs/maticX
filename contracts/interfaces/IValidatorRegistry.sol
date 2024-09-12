1ceff49b4f326304d94b502c00e803d782b6d48c
Sergey <2901744+evercoinx@users.noreply.github.com.>
2024-09-12 20:37:49 +0200

    Add POL token to ValidatorRegistry contract

--- contracts/interfaces/IValidatorRegistry.sol
+++ contracts/interfaces/IValidatorRegistry.sol
@@ -4,6 +4,20 @@ pragma solidity 0.8.7;
 /// @title IValidatorRegistry
 /// @notice Node validator registry interface
 interface IValidatorRegistry {
+	event AddValidator(uint256 indexed _validatorId);
+
+	event RemoveValidator(uint256 indexed _validatorId);
+
+	event SetPreferredDepositValidatorId(uint256 indexed _validatorId);
+
+	event SetPreferredWithdrawalValidatorId(uint256 indexed _validatorId);
+
+	event SetMaticX(address _address);
+
+	event SetVersion(string _version);
+
+	event SetPOLToken(address _address);
+
 	function addValidator(uint256 _validatorId) external;
 
 	function removeValidator(uint256 _validatorId) external;
@@ -24,28 +38,23 @@ interface IValidatorRegistry {
 
 	function preferredWithdrawalValidatorId() external view returns (uint256);
 
-	function validatorIdExists(uint256 _validatorId)
-		external
-		view
-		returns (bool);
+	function validatorIdExists(
+		uint256 _validatorId
+	) external view returns (bool);
 
 	function getContracts()
 		external
 		view
 		returns (
 			address _stakeManager,
-			address _polygonERC20,
-			address _maticX
+			address _maticToken,
+			address _maticX,
+			address _polToken
 		);
 
 	function getValidatorId(uint256 _index) external view returns (uint256);
 
 	function getValidators() external view returns (uint256[] memory);
 
-	event AddValidator(uint256 indexed _validatorId);
-	event RemoveValidator(uint256 indexed _validatorId);
-	event SetPreferredDepositValidatorId(uint256 indexed _validatorId);
-	event SetPreferredWithdrawalValidatorId(uint256 indexed _validatorId);
-	event SetMaticX(address _address);
-	event SetVersion(string _version);
+	function setPOLToken(address _address) external;
 }
