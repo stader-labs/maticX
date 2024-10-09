509884587da9b3ca4fe785e4a7c605011ec1a9a6
Sergey <2901744+evercoinx@users.noreply.github.com.>
2024-10-09 20:58:32 +0200

    Implement task to initialize v2 MaticX contract

--- tasks/index.ts
+++ tasks/index.ts
@@ -1,6 +1,7 @@
 import "./deploy-matic-x";
 import "./deploy-validator-registry";
 import "./import-contract";
+import "./initialize-v2-matic-x";
 import "./initialize-v2-validator-registry";
 import "./upgrade-contract";
 import "./verify-contract";
